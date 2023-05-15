27703d31d164ca96218b5f1812ddd68007f36093
Pang <narumonokanoto@gmail.com>
2023-05-15 14:55:19 +0700

    R1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program{
+
+    static int Compare(int a, int b) {
+        if (a <= b) {
+           return a; 
+        } 
+        return b;
+    }
+
+
+  static void Main(string[] args){
+    float volumemax = float.Parse(Console.ReadLine());
+    float volumedrink = float.Parse(Console.ReadLine());
+    //float volumefill = float.Parse(Console.ReadLine());
+    //float tineday = float.Parse(Console.ReadLine());
+    //float timedrink = float.Parse(Console.ReadLine());
+    //float timefill = float.Parse(Console.ReadLine());
+
+    //float result1 = Compare((int)volumedrink, (int)volumemax);
+    if(volumedrink <= volumemax){
+        Console.Write("Enough Water");
+        } else {
+            Console.Write("Not Enough Water");
+        }
+
+
+    }
+
+
+        }
